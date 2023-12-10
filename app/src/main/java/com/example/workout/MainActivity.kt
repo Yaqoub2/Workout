9074178f432f07787216d66a662c644b411c8b12
Yaqoub <yaqoub3007@gmail.com>
2023-12-10 18:52:38 +0300

    created start button

--- app/src/main/java/com/example/workout/MainActivity.kt
+++ app/src/main/java/com/example/workout/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.workout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.button)
+        val viewT = findViewById<TextView>(R.id.viewText)
+        button.setOnClickListener {
+            viewT.visibility = View.VISIBLE
+        }
     }
 }
